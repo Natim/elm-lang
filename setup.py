b85cb985c6d6e4b7e62e13b82de52552ba3d59cb
Rémy HUBSCHER <remy@chefclub.tv>
2019-12-05 16:19:45 +0100

    Upgrade to 0.19.1

--- setup.py
+++ setup.py
@@ -71,7 +71,7 @@ with codecs.open(os.path.join(HERE, "CONTRIBUTORS.rst"), encoding="utf-8") as f:
 
 setup(
     name="elm-lang",
-    version="0.19.0.1",
+    version="0.19.1",
     description="Elm compiler installer",
     long_description=README + "\n\n" + CHANGELOG + "\n\n" + CONTRIBUTORS,
     license="Apache License (2.0)",
